4bdeb869cccd7849b11146e4357b4ab812af04df
Romas Bitinas <93491714+pupixipup@users.noreply.github.com>
2024-03-18 10:41:46 +0100

    fix: losing scroll position on items fetch

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -59,6 +59,10 @@ function App() {
   useEffect(() => {
     fetchData()
   }, [category, sort, data])
+
+  useEffect(() => {
+    console.log(locations)
+  }, [locations])
   if (error) {
     return (
       <div>
@@ -73,9 +77,6 @@ function App() {
     )
   }
 
-  if (loading) {
-    return <h1 className="text-center">LOADING</h1>
-  }
 
   return (
     <div>
@@ -104,11 +105,10 @@ function App() {
               </Category>
             ))}
           </div>
-          {/* <hr className="m-5 mb-10" /> */}
           <hr className="ml-5 mr-5" />
           <h3 className="mt-[-15px] ml-7 font-bold text-lg">
             <span className="bg-[var(--main-bg-color)] pl-3 pr-3">
-              Alphabetical order:{" "}
+              Alphabetical order:
             </span>
           </h3>
           <div className="m-1 ml-7 mb-12 p-1 flex gap-1 overflow-x-auto">
@@ -125,13 +125,13 @@ function App() {
               </Category>
             ))}
           </div>
-          {/* <hr className="m-5 mt-2 mb-12" /> */}
 
           <div className="grid justify-items-center gap-y-20 locations_grid p-1">
             {locations.map((el) => (
               <Card key={el._id} location={el} />
             ))}
           </div>
+          {loading &&<h1 className="text-center">Loading...</h1>}
         </div>
       </div>
       {from < count && (
